9b64b5d8845847d9c3edc5d31bdc440391421447
Connor Black <connor.b645@gmail.com>
2025-04-17 08:41:30 +0100

    Upgraded Pulse

--- Package.swift
+++ Package.swift
@@ -4,31 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "Logging",
-    platforms: [
-        .iOS(.v15),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Logging",
-            targets: ["Logging"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/kean/Pulse.git", from: "5.1.2")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Logging",
-            dependencies: [
-                .product(name: "Pulse", package: "Pulse")
-            ]
-        ),
-        .testTarget(
-            name: "LoggingTests",
-            dependencies: ["Logging"]
-        ),
-    ]
+  name: "Logging",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Logging",
+      targets: ["Logging"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/kean/Pulse.git", from: "5.1.4")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Logging",
+      dependencies: [
+        .product(name: "Pulse", package: "Pulse")
+      ]
+    ),
+    .testTarget(
+      name: "LoggingTests",
+      dependencies: ["Logging"]
+    ),
+  ]
 )
